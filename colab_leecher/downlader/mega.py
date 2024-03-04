dc92ff194ec80e61f236bdf2440737ef0f1cc716
Dark Hunter <86953304+kavinduaj12@users.noreply.github.com>
2024-03-04 13:09:30 +0530

    Update mega.py

--- colab_leecher/downlader/mega.py
+++ colab_leecher/downlader/mega.py
@@ -1,35 +1,41 @@
-import logging
+# Some codes and idea were taken from https://github.com/Itz-fork/Mega.nz-Bot/tree/nightly | Thanks to  https://github.com/Itz-fork
+# So, I can't take the entire credit for this module
+
+
+import subprocess, logging
 from datetime import datetime
 from colab_leecher.utility.helper import status_bar
 from colab_leecher.utility.variables import BotTimes, Messages, Paths
-from pymegatools import Megatools, MegaError
+from pymegatools import Megatools,MegaError
 
 async def megadl(link: str, num: int):
-    global BotTimes
+
+    global BotTimes, Messages
     BotTimes.task_start = datetime.now()
-    mega = Megatools()
+    mega= Megatools()
     try:
-        await mega.async_download(link, progress=pro_for_mega, path=Paths.down_path)
+        await mega.async_download(link,progress=pro_for_mega,path= Paths.down_path)
     except MegaError as e:
-        logging.error(f"An Error occurred: {e}")
+        logging.error(f"An Error occured : {e}")
+
 
 async def pro_for_mega(stream, process):
-    line = stream[-1]
+    line =stream[-1]
     file_name = "N/A"
     percentage = 0
     downloaded_size = "N/A"
     total_size = "N/A"
     speed = "N/A"
-    try:
-        parts = line.split(":")
-        file_name = parts[0].strip()
-        ok = parts[1].split()
+    try :
+        ok =line.split(":")
+        file_name = ok[0]
+        ok=ok[1].split()
         percentage = float(ok[0][:-1])
-        downloaded_size = " ".join(ok[2:4])
-        total_size = " ".join(ok[7:9])
-        speed = " ".join(ok[9:11])
-    except Exception as e:
-        logging.error(f"Error parsing status line: {e}")
+        downloaded_size = ok[2]+" "+ok[3]
+        total_size =  ok[7]+" "+ok[8]
+        speed = ok[9][1:]+" "+ok[10][:-1]
+    except Exception:
+        pass
     Messages.download_name = file_name
     Messages.status_head = f"<b>📥 DOWNLOADING FROM MEGA » </b>\n\n<b>🏷️ Name » </b><code>{file_name}</code>\n"
     
